55bce0924c9231ee6c31328e7d0e378137c2dc3e
Kyaw Zin Hein <kyawzinhein6@gmail.com>
2024-02-06 17:01:51 +0630

    Clean up

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,6 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_news from "./routes/api/news.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $NewProvider from "./islands/NewProvider.tsx";
 import * as $News from "./islands/News.tsx";
@@ -16,7 +15,6 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/news.ts": $api_news,
-    "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
   islands: {
